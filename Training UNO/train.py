ebdcf8e1d4c7a78cb836029b86fff13a39b9123e
Yousif Alasadi <yousifbassam01@yahoo.com>
2023-04-14 12:49:31 +0200

    ''

--- Training UNO/train.py
+++ Training UNO/train.py
@@ -10,35 +10,43 @@ def resize(img, scale_percent=.05):
     return cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
 img = resize(cv.imread(r'C:\Users\eyoalxa\Documents\Python OpenCV\OpenCV Course\Photos\20230406_152117.jpg'))
-#cv.imshow('UNO', img)
 
 grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blur = cv.GaussianBlur(grey, (5,5), cv.BORDER_DEFAULT)
 
-t_lower = 100  # Lower Threshold
+t_lower = 50  # Lower Threshold
 t_upper = 200  # Upper threshold
-aperture_size = 3  # Aperture size
+aperture_size = 5  # Aperture size
 
-canny = cv.Canny(blur, 50, 150, apertureSize=aperture_size)
+canny = cv.Canny(blur, 50, 200, apertureSize=aperture_size)
+
+# Dilate the edges to close any gaps in the white outline
+kernel = np.ones((5,5),np.uint8)
+canny = cv.dilate(canny,kernel,iterations = 1)
+
 ret, thresh = cv.threshold(canny, 125, 255, cv.THRESH_BINARY)
 contours, hierarchies = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
-#cv.imshow('Canny', canny)
 
-mask = np.zeros(shape=img.shape, dtype='uint8')
+cv.imshow('th', thresh)
 
-cv.drawContours(mask, contours, -1, (255,255,255), thickness=cv.FILLED)
+# Find the longest contour
+max_contour = None
+max_length = 0
+for contour in contours:
+    length = cv.arcLength(contour, True)
+    if length > max_length:
+        max_length = length
+        max_contour = contour
 
-#cv.imshow('A', img)
+# Draw the longest contour on the mask
+mask = np.zeros(shape=img.shape[:2], dtype='uint8')
+cv.drawContours(mask, [max_contour], -1, (255,255,255), thickness=cv.FILLED)
 
-#cv.imshow('A', mask)
+# Copy the original image onto a blank image using the mask
+output = np.zeros(shape=img.shape, dtype='uint8')
+cv.copyTo(img, mask, output)
 
-blank = np.zeros(shape=img.shape, dtype='uint8')
-blank[:] = (0, 0, 0)
-
-cv.copyTo(img, mask, blank)
-cv.normalize(mask.copy(), None, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
-
-cv.imshow('Blank', blank)
-cv.imshow('Contours', mask)
-cv.imwrite(PATH, canny)
+# Show the output image
+cv.imshow('Output', output)
+cv.imwrite(PATH, output)
 cv.waitKey(0)
