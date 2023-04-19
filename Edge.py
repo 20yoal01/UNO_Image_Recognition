619bf25f26958b558cab7dd435a48469d882e642
Yousif Alasadi <yousifbassam01@yahoo.com>
2023-04-19 09:54:02 +0200

    Får fram korten / filtrerar bort onödiga konturer

--- Edge.py
+++ Edge.py
@@ -7,7 +7,7 @@ def resize(img, scale_percent=.5):
     dim = (width, height)
     return cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
-img = resize(cv.imread(r'frame.jpg'))
+img = resize(cv.imread(r'pic1.png'))
 #cv.imshow('UNO', img)
 
 grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -29,7 +29,7 @@ canny = cv.dilate(canny,kernel,iterations = 1)
 
 ret, thresh = cv.threshold(canny, 125, 255, cv.THRESH_BINARY)
 cv.imshow('thresh', thresh)
-contours, hierarchies = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) 
+contours, hierarchies = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
 
 index_sort = sorted(range(len(contours)), key=lambda i : cv.contourArea(contours[i]),reverse=True)
 
@@ -37,31 +37,40 @@ contour_sort = []
 hierarchie_sort = []
 
 mask = np.zeros(shape=img.shape, dtype='uint8')
+cnt_is_card = np.zeros(len(contours),dtype=int)
+cv.drawContours(mask, contours, -1, (255,255,255), thickness=cv.FILLED)
 
 for i in index_sort:
     contour_sort.append(contours[i])
-    hierarchie_sort.append(hierarchies[i])
+    hierarchie_sort.append(hierarchies[0][i])
+
+print(range(len(contour_sort)))
 
 for i in range(len(contour_sort)):
     size = cv.contourArea(contour_sort[i])
     peri = cv.arcLength(contour_sort[i],True)
-    approx = cv.approxPolyDP(contour_sort[i],0.01*peri,True)
+    approx = cv.approxPolyDP(contour_sort[i],0.02*peri,True)
 
-    if((size < 3000000) and (size > 20) and (hierarchie_sort[i][3] == -1) and (len(approx) == 4)):
-        mask[i] = 1
-
-
-cv.drawContours(mask, contours, -1, (255,255,255), thickness=cv.FILLED)
+    print(size)
+    print((len(approx)))
+    if((size < 120_000) and (size > 0) and (hierarchie_sort[i][3] == -1) and (len(approx) == 4)):
+        cnt_is_card[i] = 1
 
 #cv.imshow('A', img)
-
+print(cnt_is_card)
 #cv.imshow('A', mask)
 
 blank = np.zeros(shape=img.shape, dtype='uint8')
 blank[:] = (0, 0, 0)
 #blank = cv.imread('namn.jpg')
+cv.imshow('Mask', mask)
 
-cv.copyTo(img, mask, blank)
+
+for i in range(len(contour_sort)):
+    if (cnt_is_card[i] == 1):
+        cv.drawContours(blank,[contour_sort[i]], -1, (255,255,255), -1)
+
+cv.copyTo(img, blank, blank)
 cv.normalize(mask.copy(), None, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
 
 cv.imshow('Blank', blank)
