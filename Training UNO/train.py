cf4772f67ae3c10d370244d936f92e117d454a26
Yousif Alasadi <yousifbassam01@yahoo.com>
2023-04-24 15:37:02 +0200

    allt förutom hsv klar

--- Training UNO/train.py
+++ Training UNO/train.py
@@ -1,12 +1,25 @@
 import cv2 as cv
 import numpy as np
 import os
+import random
 
-UNO_CARDS = 'OpenCV Course/Photos V2/'
-IMAGES_TO_CREATE = 100
+UNO_CARDS_PATH = 'OpenCV Course/Photos V2/'
 OUTPUT = 'UNO Syn/'
-BACKGROUNDS = r'U:\[target_dir\validation]'
+BACKGROUND_PATH = 'OpenCV Course/background'
+CARD_TYPE = ['RED', 'GREEN', 'BLUE', 'YELLOW', 'WILD']
 
+
+IMAGES_PER_CARD = 100
+TOTAL_CARDS_TO_GENERATE = 1
+
+
+TARGET_HEIGHT, TARGET_WIDTH = (720, 1280)
+ROTATE_RANGE = [-180, 180]
+TRANSLATE_RANGE = [(TARGET_WIDTH//2)*0.06, (TARGET_HEIGHT//2)*0.06] # 10% of screen
+SHEAR_RANGE = (-0.35, 0.35)
+PROJECTION_RANGE = (-0.35, 0.35)
+SATURATION_RANGE = 0
+VALUE_RANGE = 0
 
 def resize(img, scale_percent=.05):
     width = int(img.shape[1] * scale_percent)
@@ -14,36 +27,66 @@ def resize(img, scale_percent=.05):
     dim = (width, height)
     return cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
-def rotate(img, angle, rotPoint=None):
-    (height, width) = img.shape[:2]
+def resize_no_aspect(img, width, height):
+    dim = (width, height)
+    return cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
-    if rotPoint is None:
-        rotPoint = (width//2, height//2)
+# def rotate(img, angle, rotPoint=None):
+#     (height, width) = img.shape[:2]
 
-    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 0.8)
-    dimensions = (width, height)
+#     if rotPoint is None:
+#         rotPoint = (width//2, height//2)
 
-    return cv.warpAffine(img, rotMat, dimensions)
+#     rotMat = cv.getRotationMatrix2D(rotPoint, angle, 0.8)
+#     dimensions = (width, height)
+
+#     return cv.warpAffine(img, rotMat, dimensions)
     
-def translate(img, x, y):
-    transMat = np.float32([[1, 0, x], [0, 1, y]])
-    dimensions = (img.shape[1], img.shape[0])
-    return cv.warpAffine(img, transMat, dimensions)
+# def translate(img, x, y):
+#     transMat = np.float32([[1, 0, x], [0, 1, y]])
+#     dimensions = (img.shape[1], img.shape[0])
+#     return cv.warpAffine(img, transMat, dimensions)
+    
 
 def shearX(img, shearFactor=0.1):
     M = np.float32([[1, shearFactor, 0], [0, 1, 0]])
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img, M, dimensions)
-
 def shearY(img, shearFactor=0.1):
     M = np.float32([[1, 0, 0], [shearFactor, 1, 0]])
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img, M, dimensions)
+
+def projectiveTransform(img, angle, translation=(0, 0), shear=(0,0), projective=(0,0), rotPoint=None):
+    (height, width) = img.shape[:2]
+    dimensions = (width, height)
+    if rotPoint is None:
+        rotPoint = (width // 2, height // 2)
     
+    # Roterar bilden
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    rotMat = np.vstack([rotMat, [0, 0, 1]])
+
+    # Shear
+    shearMat = np.float32([[1, shear[0], 0], [shear[1], 1, 0], [0, 0, 1]])
+
+    # Translation
+    transMat = np.float32([[1, 0, translation[0]], [0, 1, translation[1]], [0, 0, 1]])
+    
+    # Beräknar våran nya perspektiv matris 
+    src_pts = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
+    dst_pts = np.float32([[0, 0], [width, 0], [width + projective[0] * height, height], [-projective[1] * width, height]])
+    perspMat = cv.getPerspectiveTransform(src_pts, dst_pts)
+
+    # Kombinerar allting
+    projMat = np.dot(transMat,  np.dot(shearMat, np.dot(perspMat, rotMat)))
+    
+    return cv.warpPerspective(img, projMat, dimensions)
+
 #def changeHSV():
     
     
-
+    
 def extract_uno(img):
     grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(grey, (5,5), cv.BORDER_DEFAULT)
@@ -57,7 +100,7 @@ def extract_uno(img):
     #cv.imshow('canny', canny)
 
     # Dilate the edges to close any gaps in the white outline
-    kernel = np.ones((3,3),np.uint8)
+    kernel = np.ones((5,5),np.uint8)
     canny = cv.dilate(canny,kernel,iterations = 1)
 
     contours, hierarchies = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) 
@@ -111,33 +154,81 @@ def add_obj(background, img, mask, x, y):
     return bg
 
 
-
-for filename in os.listdir('OpenCV Course/Photos V2/'):
-    ext = os.path.splitext(filename)[-1].lower()
-
-    if ext == ".jpg":
-        filepath = os.path.join('OpenCV Course/Photos V2/', filename)
-        img = cv.imread(filepath)
-        img_resized = resize(img)
-        img_resized = shearY(img_resized)
-        img_resized = rotate(img_resized, 90)
-        #blank = np.zeros(shape=img_resized.shape, dtype='uint8')
-        #blank[:] = (255, 255, 255)
-        blank = cv.imread(r'C:\Users\ejestxa\Documents\img\UNO_Image_Recognition\Training UNO\namn.jpg')
-        down_width = 346
-        down_height = 462
-        down_points = (down_width, down_height)
-        #blank = cv.resize(blank, down_points, interpolation=cv.INTER_AREA)
-        mask = extract_uno(img_resized)
-
-        pic = add_obj(blank, img_resized, mask, 200, 300)
-        #h, w = mask.shape[:2]
-        #x_offset = 50
-        #y_offset = 0
+index = 0
+folder_index = 0
+CURRENT_UNO_PATH = UNO_CARDS_PATH + CARD_TYPE[index]
+total_cards = 0
+while True:
+    if total_cards >= TOTAL_CARDS_TO_GENERATE:
+        break
+    
+    total_cards += 1
+    
+    filedir = os.listdir(CURRENT_UNO_PATH)
+    
+    if folder_index >= len(filedir) and index < len(CARD_TYPE):
+        index += 1
+        if index >= len(CARD_TYPE):
+            break
+        CURRENT_UNO_PATH = UNO_CARDS_PATH + CARD_TYPE[index]
+        filedir = os.listdir(CURRENT_UNO_PATH)
+        folder_index = 0
+    
+    filename = filedir[folder_index]    
+    
+    cardType = CARD_TYPE[index] + " " + filename[:]
+    print(cardType)
+    
+    folder_index += 1
+    
+    img_to_create = 0
+    
+    for background in os.listdir(BACKGROUND_PATH):
+        if img_to_create >= IMAGES_PER_CARD:
+            break
         
-        #blank[y_offset:y_offset+h, x_offset:x_offset+w] = mask
-        #cv.copyTo(img_resized,mask,blank)
+        img_to_create += 1
         
-        cv.imshow("UNO", pic)
-        cv.waitKey(0)
-        cv.destroyAllWindows() 
+        ext = os.path.splitext(filename)[-1].lower()
+        if ext == ".jpg":
+            background_img_path = os.path.join(BACKGROUND_PATH, background)
+            
+            rotation     = round(random.uniform(ROTATE_RANGE[0], ROTATE_RANGE[1]), 2)
+            translate_x  = round(random.uniform(-TRANSLATE_RANGE[0], TRANSLATE_RANGE[0]), 2)
+            translate_y  = round(random.uniform(-TRANSLATE_RANGE[1], TRANSLATE_RANGE[1]), 2)
+            shear_x      = round(random.uniform(SHEAR_RANGE[0], SHEAR_RANGE[1]), 2)
+            shear_y      = round(random.uniform(SHEAR_RANGE[0], SHEAR_RANGE[1]), 2)
+            projection_x = round(random.uniform(PROJECTION_RANGE[0], PROJECTION_RANGE[1]), 2)
+            projection_y = round(random.uniform(PROJECTION_RANGE[0], PROJECTION_RANGE[1]), 2)
+            saturation   = round(random.uniform(SATURATION_RANGE, SATURATION_RANGE), 2)
+            value        = round(random.uniform(VALUE_RANGE, VALUE_RANGE), 2)
+
+            print(rotation, translate_x, translate_y, shear_x, shear_y, projection_x, projection_y, saturation, value)
+
+            filepath = os.path.join(CURRENT_UNO_PATH, filename)
+            img = cv.imread(filepath)
+            img_resized = resize(img, scale_percent=0.025)
+
+            blank = np.zeros(shape=(TARGET_HEIGHT, TARGET_WIDTH, 3), dtype='uint8')
+            blank[:] = (0, 0, 0)
+            
+            background_img = cv.imread(background_img_path)
+            b_dimensions = background_img.shape[:2]
+
+            background_img = resize_no_aspect(background_img, TARGET_WIDTH, TARGET_HEIGHT)
+            mask = extract_uno(img_resized)
+            #cv.imshow('m', mask)
+            pic = add_obj(blank, img_resized, mask, TARGET_WIDTH//2, TARGET_HEIGHT//2)
+            pic = projectiveTransform(pic, rotation, (translate_x, translate_y), (shear_x, shear_y), (projection_x, projection_y))
+
+            pic_gray = cv.cvtColor(pic, cv.COLOR_BGR2GRAY)
+            ret, pic_mask = cv.threshold(pic_gray, 10, 255, cv.THRESH_BINARY)
+            background_mask = cv.bitwise_not(pic_mask)
+            card_masked = cv.bitwise_and(pic, pic, mask=pic_mask)
+            background_masked = cv.bitwise_and(background_img, background_img, mask=background_mask)
+            pic = cv.add(card_masked, background_masked)
+
+            cv.imwrite('test/' + background + '_' + cardType, pic)
+            #cv.imshow("UNO", pic)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
