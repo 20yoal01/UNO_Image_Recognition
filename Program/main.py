491ccb1e721bd7d92c8c9295ff5728ec2aadc8a8
Jennifer Streit <jenniferstreit@hotmail.com>
2023-05-03 13:43:56 +0200

    genererat testdata

--- Program/main.py
+++ Program/main.py
@@ -1,34 +1,85 @@
 import cv2 as cv
 import numpy as np
-from collections import OrderedDict
-from scipy.spatial import distance as dist
+import matplotlib.pyplot as plt
+import os
+import random
 
 LOWER_THRESHOLD = 0
 UPPER_THRESHOLD = 255
 APERTURE_SIZE = 3
+UNO_CARD_PATH = 'photos/yellow/'
+UNO_TYPE = 'yellow'
+SATURATION_RANGE = (0, 100)
+VALUE_RANGE = (-30, 255)
 
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-yellow = (255,255,0)
-black = (0,0,0)
+def changeHSV(img, saturation, value):
+    changeImg = img.copy()
+    changeImg = cv.cvtColor(changeImg, cv.COLOR_BGR2HSV).astype("float32")
+    h,s,v = cv.split(changeImg)
+    lim = 255 - value 
+    v[v > lim] = 255 
+    v[v <= lim] += value
+    lim = 255 - saturation 
+    s[s > lim] = 255 
+    s[s <= lim] += saturation
+    changeImg = cv.merge([h,s,v])
+    changeImg = cv.cvtColor(changeImg.astype("uint8"), cv.COLOR_HSV2BGR)
+    return changeImg
 
-img = cv.imread(r'C:\Users\ejestxa\Documents\img\UNO_Image_Recognition\photos\20230414_143429.jpg')
-width = int(img.shape[1] * 0.05)
-height = int(img.shape[0] * 0.05)
-dim = (width, height)
-img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+file_dir = os.listdir(UNO_CARD_PATH)
+loop_range = int(100/len(file_dir))
+color_array = []
 
-blur = cv.GaussianBlur(img_gray, (7,7), cv.BORDER_DEFAULT)
-canny = cv.Canny(blur, LOWER_THRESHOLD, UPPER_THRESHOLD, apertureSize=APERTURE_SIZE)
+for file in file_dir:
+    file_path = os.path.join(UNO_CARD_PATH,file)
+    img = cv.imread(file_path)
+    img = cv.resize(img, None, fx= 0.03, fy= 0.03, interpolation=cv.INTER_AREA)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-kernel = np.ones((5,5),np.uint8)
-canny = cv.dilate(canny,kernel,iterations = 1)
-contours, hierarchies = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) 
+    blur = cv.GaussianBlur(img_gray, (7,7), cv.BORDER_DEFAULT)
+    canny = cv.Canny(blur, LOWER_THRESHOLD, UPPER_THRESHOLD, apertureSize=APERTURE_SIZE)
 
-mask = np.zeros(img.shape[:2], dtype='uint8')
-cv.drawContours(mask, contours, -1, (255,255,255), -1)
-mask = cv.erode(mask,None, iterations=2)
-mask = cv.mean(img, mask=mask)[:3]
-print(mask) #färg i bgr (eventuellt)
+    kernel = np.ones((5,5),np.uint8)
+    canny = cv.dilate(canny,kernel,iterations = 1)
+    contours, hierarchies = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) 
+
+    mask = np.zeros(img.shape[:2], dtype='uint8')
+    cv.drawContours(mask, contours, -1, (255,255,255), -1)
+    mask = cv.erode(mask, None, iterations=2)
+
+    mean_color = cv.mean(img, mask=mask)[:3]
+    color_array.insert(len(color_array)-1,[str(int(mean_color[0])),str(int(mean_color[1])),str(int(mean_color[2])),UNO_TYPE])
+
+for x in range(loop_range):
+    for file in file_dir:
+        file_path = os.path.join(UNO_CARD_PATH,file)
+        print(file_path)
+        img = cv.imread(file_path)
+        img = cv.resize(img, None, fx= 0.03, fy= 0.03, interpolation=cv.INTER_AREA)
+        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        blur = cv.GaussianBlur(img_gray, (7,7), cv.BORDER_DEFAULT)
+        canny = cv.Canny(blur, LOWER_THRESHOLD, UPPER_THRESHOLD, apertureSize=APERTURE_SIZE)
+
+        kernel = np.ones((5,5),np.uint8)
+        canny = cv.dilate(canny,kernel,iterations = 1)
+        contours, hierarchies = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) 
+
+        mask = np.zeros(img.shape[:2], dtype='uint8')
+        cv.drawContours(mask, contours, -1, (255,255,255), -1)
+        mask = cv.erode(mask, None, iterations=2)
+
+        saturation = round(random.uniform(SATURATION_RANGE[0], SATURATION_RANGE[1]), 2)
+        value = round(random.uniform(VALUE_RANGE[0], VALUE_RANGE[1]), 2)
+        img = changeHSV(img,saturation,value)
+
+        mean_color = cv.mean(img, mask=mask)[:3]
+        color_array.insert(len(color_array)-1,[str(int(mean_color[0])),str(int(mean_color[1])),str(int(mean_color[2])),UNO_TYPE])
+        print(mean_color)
+
+    print(str(x) + "/" + str(loop_range))
+    
+
+print(color_array)
+arr = np.array(color_array)
+np.savetxt('yellow.csv',arr,fmt="%s",delimiter=",")
