89ba9b4b56ce5868a80e3fd5b36041a8ada77d87
Jennifer Streit <jenniferstreit@hotmail.com>
2023-05-05 09:46:34 +0200

    klar

--- Program/testt.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-blue_responses = np.zeros(10, dtype=np.float32)
-red_resposnes = np.ones(10, dtype=np.float32)
-responses = np.concatenate((blue_responses, red_resposnes))
-
-print(responses)
