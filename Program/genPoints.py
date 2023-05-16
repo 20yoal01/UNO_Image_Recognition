f398effd9d3f8ec41600d61e649c1ee80041082e
Jennifer Streit <jenniferstreit@hotmail.com>
2023-05-16 19:25:08 +0200

    nya bilder

--- Program/genPoints.py
+++ Program/genPoints.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 import os
 import random
 
-LOWER_THRESHOLD = 0
+LOWER_THRESHOLD = 100
 UPPER_THRESHOLD = 255
-APERTURE_SIZE = 3
-UNO_CARD_PATH = 'photos/yellow/'
-UNO_TYPE = 'yellow'
+APERTURE_SIZE = 5
+UNO_CARD_PATH = 'photos/new/yellow/'
 SATURATION_RANGE = (0, 30)
 VALUE_RANGE = (0, 40)
 
@@ -33,22 +32,50 @@ color_array = []
 for file in file_dir:
     file_path = os.path.join(UNO_CARD_PATH,file)
     img = cv.imread(file_path)
-    img = cv.resize(img, None, fx= 0.03, fy= 0.03, interpolation=cv.INTER_AREA)
+    img = cv.resize(img, None, fx= 0.5, fy= 0.5, interpolation=cv.INTER_AREA)
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    blur = cv.GaussianBlur(img_gray, (7,7), cv.BORDER_DEFAULT)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blur = cv.medianBlur(gray, 11)
+    blur = cv.bilateralFilter(blur, 9, 130, 130)
+        
     canny = cv.Canny(blur, LOWER_THRESHOLD, UPPER_THRESHOLD, apertureSize=APERTURE_SIZE)
+    karnel = np.ones((5,5), np.uint8)
+    canny = cv.dilate(canny, karnel,iterations=1)
+    ret, threshold = cv.threshold(canny, 125, 255, cv.THRESH_BINARY)
+    contours, hier = cv.findContours(threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    
+    mask = np.zeros(shape=img.shape[:2], dtype=np.uint8)
 
-    kernel = np.ones((5,5),np.uint8)
-    canny = cv.dilate(canny,kernel,iterations = 1)
-    contours, hierarchies = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) 
+    index_sort = sorted(range(len(contours)), key=lambda i : cv.contourArea(contours[i]), reverse=True)
 
-    mask = np.zeros(img.shape[:2], dtype='uint8')
-    cv.drawContours(mask, contours, -1, (255,255,255), -1)
-    mask = cv.erode(mask, None, iterations=2)
-    blank = np.ones(img.shape[:2], dtype='uint8')
+    cnt_sorted = []
+    hier_sorted = []
+    cards = []
+
+    contour_is_card = np.zeros(len(contours), dtype=int)
+    
+    for i in index_sort:
+        cnt_sorted.append(contours[i])
+        hier_sorted.append(hier[0][i])
+    
+    for i in range(len(contours)):
+        size = cv.contourArea(cnt_sorted[i])
+        peri = cv.arcLength(cnt_sorted[i],True)
+        approx = cv.approxPolyDP(cnt_sorted[i],0.02*peri,True)
+        if((size < 120_000) and (size > 0) and (hier_sorted[i][3] == -1) and (len(approx) == 4)):
+            contour_is_card[i] = 1
+
+    print(contour_is_card)
+
+    for i in range(len(cnt_sorted)):
+        if (contour_is_card[i] == 1):
+            cv.drawContours(mask, [cnt_sorted[i]], -1, (255,225,225), -1)
     #img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV).astype(np.float32)
     #print(img_hsv)
+    mask = cv.erode(mask, None, iterations=2)
+    cv.imshow('d',mask)
+    cv.waitKey(0)
     mean_color = cv.mean(img, mask=mask)[:3]
     color_array.append([str(int(mean_color[0])),str(int(mean_color[1])),str(int(mean_color[2]))])
 
@@ -61,4 +88,4 @@ for file in file_dir:
         color_array.append([str(int(mean_color[0])),str(int(mean_color[1])),str(int(mean_color[2]))])
 
 arr = np.array(color_array)
-np.savetxt('yellow_bgr.csv', arr, fmt="%s", delimiter=",")
+np.savetxt('new_yellow_bgr.csv', arr, fmt="%s", delimiter=",")
--- Program/plot.py
+++ Program/plot.py
@@ -2,11 +2,17 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-blue = np.loadtxt('blue_hsv.csv', delimiter=",", dtype=int)
-green = np.loadtxt('green_hsv.csv', delimiter=",", dtype=int)
-red = np.loadtxt('red_hsv.csv', delimiter=",", dtype=int)
-wild = np.loadtxt('wild_hsv.csv', delimiter=",", dtype=int)
-yellow = np.loadtxt('yellow_hsv.csv', delimiter=",", dtype=int)
+blue = np.loadtxt('new_blue_bgr.csv', delimiter=",", dtype=int)
+green = np.loadtxt('new_green_bgr.csv', delimiter=",", dtype=int)
+red = np.loadtxt('new_red_bgr.csv', delimiter=",", dtype=int)
+wild = np.loadtxt('new_wild_bgr.csv', delimiter=",", dtype=int)
+yellow = np.loadtxt('new_yellow_bgr.csv', delimiter=",", dtype=int)
+blue2 = np.loadtxt('blue_bgr.csv', delimiter=",", dtype=int)
+green2 = np.loadtxt('green_bgr.csv', delimiter=",", dtype=int)
+red2 = np.loadtxt('red_bgr.csv', delimiter=",", dtype=int)
+wild2 = np.loadtxt('wild_bgr.csv', delimiter=",", dtype=int)
+yellow2 = np.loadtxt('yellow_bgr.csv', delimiter=",", dtype=int)
+
 
 print(blue[:,0])
  
@@ -20,11 +26,17 @@ ax.scatter3D(green[:,0], green[:,1], green[:,2], color = "green")
 ax.scatter3D(red[:,0], red[:,1], red[:,2], color = "red")
 ax.scatter3D(wild[:,0], wild[:,1], wild[:,2], color = "black")
 ax.scatter3D(yellow[:,0], yellow[:,1], yellow[:,2], color = "yellow")
-ax.scatter3D([0,0,0,180,0,180,180,180],[0,0,255,0,255,255,255,0],[0,255,0,0,255,0,255,255], color="white" )
+ax.scatter3D(blue2[:,0], blue2[:,1], blue2[:,2], color = "blue")
+ax.scatter3D(green2[:,0], green2[:,1], green2[:,2], color = "green")
+ax.scatter3D(red2[:,0], red2[:,1], red2[:,2], color = "red")
+ax.scatter3D(wild2[:,0], wild2[:,1], wild2[:,2], color = "black")
+ax.scatter3D(yellow2[:,0], yellow2[:,1], yellow2[:,2], color = "yellow")
+ax.scatter3D(103, 134, 137, color="magenta")
+ax.scatter3D([0,0,0,255,0,255,255,255],[0,0,255,0,255,255,255,0],[0,255,0,0,255,0,255,255], color="white" )
 
-ax.set_xlabel('hue', fontweight ='bold')
-ax.set_ylabel('saturation', fontweight ='bold')
-ax.set_zlabel('value', fontweight ='bold')
+ax.set_xlabel('blue', fontweight ='bold')
+ax.set_ylabel('green', fontweight ='bold')
+ax.set_zlabel('red', fontweight ='bold')
 plt.title("simple 3D scatter plot")
  
 
