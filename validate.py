206f7417514274aa81f2952486c65f15a7cd7dc4
Yousif Alasadi <yousifbassam01@yahoo.com>
2023-05-22 14:46:22 +0200

    KNN ändringar

---
+++ validate.py
@@ -0,0 +1,39 @@
+import YOLOv5_UNO.detect_CNN_KNN as detect_CNN_KNN
+import YOLOv5_UNO.detect_CNN as detect_CNN
+import threading
+
+print('UNO Card Detector press one of these numbers to start!')
+print('1: Template Matching with Color')
+print('2: Template Matching with KNN')
+print('3: CNN with Color')
+print('4: CNN with KNN')
+
+#
+value = None
+detection = None
+
+while True:
+    value = input()
+    try:
+       value = int(value)
+    except ValueError:
+       print ('Invalid range, please enter: 1-4')
+       continue
+    if 1 <= value <= 4:
+       break
+    else:
+       print ('Invalid range, please enter: 1-4')
+       
+match(value):
+    case 1:
+        detection = detect_CNN_KNN.ObjectDetection()
+        detection()
+    case 2:
+        detection = detect_CNN.ObjectDetection()
+        detection()
+    case 3:
+        detection = detect_CNN.ObjectDetection()
+        detection()  
+    case 4:
+        detection = detect_CNN.ObjectDetection()
+        detection()  
